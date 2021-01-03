3fe5d4ab9bdb710e2874b4b259b4f4f2eb4ef68a
Tom Gallacher <tgallacher@users.noreply.github.com>
2021-01-03 20:42:46 +0000

    feat(heating): add history request

--- src/hive/heating.ts
+++ src/hive/heating.ts
@@ -1,4 +1,29 @@
+import merge from 'lodash.merge';
+import { sub, getTime } from 'date-fns';
+import qs from 'query-string';
+
+import { BEEKEEPER_URL } from './constants';
 import { Products } from './products';
+
+interface HistoryOptions {
+  start: number; // unix_ms
+  end: number; // unix_ms
+  operation: 'AVG' | 'MIN' | 'MAX';
+  interval: {
+    type: 'MINUTES' | 'DAYS' | 'MONTHS';
+    sample: number;
+  };
+}
+
+interface HiveHomeApiHistoryDataPoint {
+  date: number; // unix_ms
+  temperature: number;
+}
+
+interface HiveSDKHistoryDataPoint {
+  timestamp: number; // unix
+  termpareature: number;
+}
 
 export class Heating extends Products {
   public async get() {
@@ -7,12 +32,45 @@ export class Heating extends Products {
     return products.filter(p => p.type === 'heating');
   }
 
-  // public async history(id: string) {
-  // https://beekeeper-uk.hivehome.com/1.0/history/heating/<id>?
-  // start=<unix>
-  // &end=<unix>
-  // &timeUnit=<MINUTES|DAYS|MONTHS>
-  // &rate=<interval>
-  // &operation=<AVG|MIN|MAX>
-  // }
+  public async history(
+    id: string,
+    options: Partial<HistoryOptions> = {}
+  ): Promise<HiveSDKHistoryDataPoint[]> {
+    const defaultOpts: HistoryOptions = {
+      start: getTime(sub(Date.now(), { days: 1 })),
+      end: Date.now(),
+      operation: 'AVG',
+      interval: {
+        type: 'MINUTES',
+        sample: 30,
+      },
+    };
+    const q = merge(defaultOpts, options) as HistoryOptions;
+
+    const url = qs.stringifyUrl({
+      url: `${BEEKEEPER_URL}/history/heating/${id}`,
+      query: {
+        start: q.start,
+        end: q.end,
+        timeUnit: q.interval.type,
+        rate: q.interval.sample,
+        operation: q.operation,
+      },
+    });
+
+    const { data } = (await this.fetch.get(url)) as {
+      data: HiveHomeApiHistoryDataPoint[];
+    };
+
+    return this.transformHistory(data);
+  }
+
+  private transformHistory(
+    data: HiveHomeApiHistoryDataPoint[]
+  ): HiveSDKHistoryDataPoint[] {
+    return data.map(d => ({
+      timestamp: Math.round(d.date / 1000),
+      termpareature: Number.parseFloat(Number(d.temperature).toFixed(2)),
+    }));
+  }
 }
--- src/hive/index.ts
+++ src/hive/index.ts
@@ -17,25 +17,4 @@ export class Hive {
     this.products = new Products(fetch);
     this.heating = new Heating(fetch);
   }
-
-  // public async getCurrentTemp() {
-  //   try {
-  //     const products = await this.getProducts();
-
-  //     const heatingProduct: Record<string, any> | undefined = products
-  //       .filter(p => p.type === 'heating') // Only `heating` products give us the temp
-  //       .shift(); // TODO: Need to support more than 1 device?
-
-  //     if (!heatingProduct) {
-  //       throw new Error(
-  //         `No heating product found! Number of all products found = ${products.length}`
-  //       );
-  //     }
-
-  //     // return heatingProduct?.props?.temperature;
-  //     return heatingProduct;
-  //   } catch (error) {
-  //     throw error;
-  //   }
-  // }
 }
